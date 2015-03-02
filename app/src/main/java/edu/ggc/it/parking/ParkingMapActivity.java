88aa2c5ff2b43c25cfe270d13d6828d333116f50
matt-the-wizard <mattberger99@gmail.comm>
2015-03-02 14:37:19 -0500

    worked on map

--- app/src/main/java/edu/ggc/it/parking/ParkingMapActivity.java
+++ app/src/main/java/edu/ggc/it/parking/ParkingMapActivity.java
@@ -5,16 +5,34 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 import edu.ggc.it.R;
 
 public class ParkingMapActivity extends Activity
 {
+    private MapFragment mapFragment;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_map);
+        mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        map = mapFragment.getMap();
+        map.setBuildingsEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(40.76793169992044, -73.98180484771729));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+
+        map.moveCamera(center);
+        map.animateCamera(zoom);
     }
 
 
