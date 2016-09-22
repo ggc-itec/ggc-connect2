55b72cd002f1b77db18aeb44ac243adfd88c377e
Ethan Patterson <Ethan Patterson>
2016-09-22 17:59:15 -0400

    first commit ethan

--- app/src/main/java/edu/ggc/it/mainscreen/MainScreenSocialView.java
+++ app/src/main/java/edu/ggc/it/mainscreen/MainScreenSocialView.java
@@ -18,7 +18,7 @@ import edu.ggc.it.rss.RSSFeed;
 /**
  * Created by gregwesterfield on 10/21/13.
  */
-public class MainScreenSocialView extends LinearLayout implements View.OnClickListener {
+public class MainScreenSocialView extends LinearLayout {
     private Context context;
 
     public MainScreenSocialView(Context context) {
@@ -36,58 +36,51 @@ public class MainScreenSocialView extends LinearLayout implements View.OnClickLi
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.main_screen_social_view, this, true);
-
-        ImageButton facebookButton = getListenedImageButton(R.id.facebook_page);
-        ImageButton twitterButton = getListenedImageButton(R.id.twitter_page);
-        ImageButton youtubeButton = getListenedImageButton(R.id.youtube_page);
-        ImageButton rssButton = getListenedImageButton(R.id.rss_feed);
     }
 
-    private ImageButton getListenedImageButton(final int resource) {
-        ImageButton temp = (ImageButton) findViewById(resource);
-        temp.setOnClickListener(this);
-        return temp;
-    }
+//    private ImageButton getListenedImageButton(final int resource) {
+//        ImageButton temp = (ImageButton) findViewById(resource);
+//        temp.setOnClickListener(this);
+//        return temp;
+//    }
 
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.facebook_page) {
-            try {
-                context.getPackageManager().getPackageInfo(
-                        "com.facebook.katana", 0);
-                context.startActivity(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("fb://profile/78573401446")));
-            } catch (Exception e) {
-                context.startActivity(new Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("http://www.facebook.com/georgiagwinnett")));
-            }
-
-        } else if (view.getId() == R.id.twitter_page) {
-            try {
-                context.getPackageManager().getPackageInfo(
-                        "com.twitter.android", 0);
-                context.startActivity(new Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("twitter://user?screen_name=georgiagwinnett")));
-            } catch (Exception e) {
-                context.startActivity(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://twitter.com/georgiagwinnett")));
-            }
-        } else if (view.getId() == R.id.youtube_page) {
-            try {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setPackage("com.google.android.youtube");
-                intent.setData(Uri
-                        .parse("http://www.youtube.com/user/georgiagwinnett"));
-                context.startActivity(intent);
-            } catch (Exception e) {
-                context.startActivity(new Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("http://www.youtube.com/user/georgiagwinnett")));
-            }
-        } else if (view.getId() == R.id.rss_feed) {
-            context.startActivity(new Intent(context, RSSActivity.class));
-        }
-    }
+//    @Override
+//    public void onClick(View view) {
+//        if (view.getId() == R.id.facebook_page) {
+//            try {
+//                context.getPackageManager().getPackageInfo(
+//                        "com.facebook.katana", 0);
+//                context.startActivity(new Intent(Intent.ACTION_VIEW,
+//                        Uri.parse("fb://profile/78573401446")));
+//            } catch (Exception e) {
+//                context.startActivity(new Intent(
+//                        Intent.ACTION_VIEW,
+//                        Uri.parse("http://www.facebook.com/georgiagwinnett")));
+//            }
+//
+//        } else if (view.getId() == R.id.twitter_page) {
+//            try {
+//                context.getPackageManager().getPackageInfo(
+//                        "com.twitter.android", 0);
+//                context.startActivity(new Intent(
+//                        Intent.ACTION_VIEW,
+//                        Uri.parse("twitter://user?screen_name=georgiagwinnett")));
+//            } catch (Exception e) {
+//                context.startActivity(new Intent(Intent.ACTION_VIEW,
+//                        Uri.parse("https://twitter.com/georgiagwinnett")));
+//            }
+//        } else if (view.getId() == R.id.youtube_page) {
+//            try {
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setPackage("com.google.android.youtube");
+//                intent.setData(Uri
+//                        .parse("http://www.youtube.com/user/georgiagwinnett"));
+//                context.startActivity(intent);
+//            } catch (Exception e) {
+//                context.startActivity(new Intent(
+//                        Intent.ACTION_VIEW,
+//                        Uri.parse("http://www.youtube.com/user/georgiagwinnett")));
+//            }
+//        }
+//    }
 }
